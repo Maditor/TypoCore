43b7ea1ccabf27a562abcb5c6e2b126e06331722
Maditor <dantruong37990@gmail.com>
2026-05-08 00:39:35 +0700

    Add files via upload

---
+++ Install_win.bat
@@ -0,0 +1,49 @@
+@echo off
+echo ========================================
+echo   TypoCore Panel V1.1 - Installer
+echo ========================================
+echo.
+echo Close Photoshop (if it is open).
+echo.
+PAUSE
+
+setlocal EnableDelayedExpansion
+
+REM Kích hoạt PlayerDebugMode cho CS6 trở lên (hỗ trợ CC 2018+)
+for /l %%x in (6, 1, 12) do (
+    reg query HKEY_CURRENT_USER\SOFTWARE\Adobe\CSXS.%%x 2>nul
+    if !errorlevel! equ 0 (
+        reg add HKEY_CURRENT_USER\SOFTWARE\Adobe\CSXS.%%x /t REG_SZ /v PlayerDebugMode /d 1 /f
+    )
+)
+
+REM Đặt đường dẫn cài đặt theo chuẩn Adobe CEP
+set TARGET_DIR=%APPDATA%\Adobe\CEP\extensions\com.typocore.panel
+
+REM Xoá cài đặt cũ nếu có
+if exist "%TARGET_DIR%" rmdir "%TARGET_DIR%" /S /Q
+
+REM Tạo thư mục đích
+md "%TARGET_DIR%"
+
+echo Installing core files...
+
+REM Copy các file gốc
+copy /Y index.html "%TARGET_DIR%\"
+copy /Y CSInterface.js "%TARGET_DIR%\"
+if exist previewWindow.html copy /Y previewWindow.html "%TARGET_DIR%\"
+if exist .debug copy /Y .debug "%TARGET_DIR%\"
+
+REM Copy các thư mục con
+if exist jsx xcopy jsx "%TARGET_DIR%\jsx\" /E /Y /I
+if exist CSXS xcopy CSXS "%TARGET_DIR%\CSXS\" /E /Y /I
+if exist icons xcopy icons "%TARGET_DIR%\icons\" /E /Y /I
+
+echo.
+echo ========================================
+echo   Installation completed!
+echo   Open Photoshop and go to:
+echo   [Window] ^> [Extensions] ^> [TypoCore]
+echo ========================================
+echo.
+PAUSE
